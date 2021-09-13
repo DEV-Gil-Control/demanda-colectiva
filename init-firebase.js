b0c6ba55327cf3318827bb48f303dc489ee0eacb
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-13 15:02:12 -0500

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,4 +1,11 @@
-// Your web app's Firebase configuration
+ <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyCXhgkjKKZvoc7TD6BX-4zf8xOI0mbPvgw",
@@ -13,3 +20,4 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+</script>
