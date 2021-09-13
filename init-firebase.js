3771fff0aac324a85f3c827f7bdac6c9a736e11d
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-12 20:39:14 -0500

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyA3Jlf8TpOjo4iJqOREIRNnvJeD68R0Ujs",
-  authDomain: "usuarios-24c65.firebaseapp.com",
-  databaseURL: "https://usuarios-24c65.firebaseio.com",
-  projectId: "usuarios-24c65",
-  storageBucket: "",
-  messagingSenderId: "110373408009",
-  appId: "1:110373408009:web:e331ab0f611b6ab3"
+apiKey: "AIzaSyCXhgkjKKZvoc7TD6BX-4zf8xOI0mbPvgw",
+    authDomain: "colectiva-a9328.firebaseapp.com",
+    projectId: "colectiva-a9328",
+    storageBucket: "colectiva-a9328.appspot.com",
+    messagingSenderId: "603650161337",
+    appId: "1:603650161337:web:73cfdd27916cd070b8f968",
+    measurementId: "G-DCN72MSJ5W"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
