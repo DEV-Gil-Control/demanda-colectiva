9b0067ad7ce9ffb24ce6e08cf305ced66c9dc3a4
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-13 14:55:27 -0500

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,23 +1,15 @@
 // Your web app's Firebase configuration
-//var firebaseConfig = {
-//apiKey: "AIzaSyCXhgkjKKZvoc7TD6BX-4zf8xOI0mbPvgw",
-  //  authDomain: "colectiva-a9328.firebaseapp.com",
- //   projectId: "colectiva-a9328",
-  //  storageBucket: "colectiva-a9328.appspot.com",
-  //  messagingSenderId: "603650161337",
- //   appId: "1:603650161337:web:73cfdd27916cd070b8f968",
- //   measurementId: "G-DCN72MSJ5W"
-//};
-// Initialize Firebase
-//firebase.initializeApp(firebaseConfig);
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCXhgkjKKZvoc7TD6BX-4zf8xOI0mbPvgw",
+    authDomain: "colectiva-a9328.firebaseapp.com",
+    projectId: "colectiva-a9328",
+    storageBucket: "colectiva-a9328.appspot.com",
+    messagingSenderId: "603650161337",
+    appId: "1:603650161337:web:d635f1db1087bf92b8f968",
+    measurementId: "G-K01MZVNKYC"
+  };
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCXhgkjKKZvoc7TD6BX-4zf8xOI0mbPvgw",
-  authDomain: "colectiva-a9328.firebaseapp.com",
-  projectId: "colectiva-a9328",
-  storageBucket: "colectiva-a9328.appspot.com",
-  messagingSenderId: "603650161337",
-  appId: "1:603650161337:web:73cfdd27916cd070b8f968",
-  measurementId: "G-DCN72MSJ5W"
-};
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
