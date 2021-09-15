eee9a263c8b65d1186c4ab8a977816224c4c2a6a
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-15 09:18:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -28,29 +28,29 @@ function login() {
   });email-password.html
 }
 
-//function observer(){
- // firebase.auth().onAuthStateChanged(function(user) {
-  //  if (user) {
-    //  show(user);
- //     // User is signed in.
-   //   var displayName = user.displayName;
-   //   var email = user.email;
-    //  var emailVerified = user.emailVerified;
-   //  var photoURL = user.photoURL;
-   //   var isAnonymous = user.isAnonymous;
-  //    var uid = user.uid;
-   //   var providerData = user.providerData;
-  //    // ...
-   //   console.log(`Usuario activo: ${email}, Estado: ${emailVerified}`)
- //   } else {
- //     console.log('Ningun Usuario Activo')
- //     content.innerHTML = ""
+function observer(){
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      show(user);
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+      console.log(`Usuario activo: ${email}, Estado: ${emailVerified}`)
+    } else {
+      console.log('Ningun Usuario Activo')
+      content.innerHTML = ""
       // User is signed out.
       // ...
-//    }
-// });
-//}
-//observer();
+    }
+  });
+}
+observer();
 
 function singOut(){
   firebase.auth().signOut().then(function() {
