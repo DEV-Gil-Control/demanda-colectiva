5df92b3734d6cae252d5ff726f9d59f089cae4c0
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-14 22:43:28 -0500

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,10 +1,4 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyBFEGUWXOTv5Sgcae53-MUJmqlPce0YZ0M",
     authDomain: "amparo-671eb.firebaseapp.com",
@@ -14,7 +8,4 @@
     appId: "1:680179086675:web:683ba04a352afcd03d1da6"
   };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
 
