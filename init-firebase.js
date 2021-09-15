b3132b41886b646fe22d19807e0bc1a40d6f1cc5
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-15 08:53:38 -0500

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,6 +1,7 @@
 const firebaseConfig = {
     apiKey: "AIzaSyBFEGUWXOTv5Sgcae53-MUJmqlPce0YZ0M",
     authDomain: "amparo-671eb.firebaseapp.com",
+    databaseURL: "https://amparo-671eb.firebaseio.com",
     projectId: "amparo-671eb",
     storageBucket: "amparo-671eb.appspot.com",
     messagingSenderId: "680179086675",
